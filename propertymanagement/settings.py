ae822fcf172f623aec69a94bd68952f9a8ce2a0f
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-10-06 18:45:55 +0500

    push git changes some files All changes

--- propertymanagement/settings.py
+++ propertymanagement/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     }
 }
 
-DATABASES = { 'default': dj_database_url.config() }
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
