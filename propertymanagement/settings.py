2a175508184da492707c7725072c885bd697b5b7
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-10-06 21:04:48 +0500

    push git changes some file

--- propertymanagement/settings.py
+++ propertymanagement/settings.py
@@ -97,11 +97,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
+DATABASE_URL = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
