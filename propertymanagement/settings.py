ade5357c390103d31934203c9f465e8692d85e70
mhaneefleghari9 <m.haneef1966@gmail.com>
2023-10-06 19:49:18 +0500

    push git changes some files All changes

--- propertymanagement/settings.py
+++ propertymanagement/settings.py
@@ -148,6 +148,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+
 
 # settings.py
 
